16df9d273c7266cafdc1035ebe918ae44ad4e36a
Thieu Nguyen <thieu@bluegrassdigital.com>
2020-07-06 14:11:39 +0700

    Use english_words lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final wordPair = new WordPair.random();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Learning Flutter',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Title'),
+        ),
+        body: new Center(
+          child: new Text(
+              wordPair.asPascalCase,
+            style: new TextStyle(fontSize: 30),
+          ),
+        )
       ),
-      home: MyHomePage(title: 'Thieu Mao'),
     );
   }
 }
+
+//class MyApp extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      title: 'Learning Flutter',
+//      theme: ThemeData(
+//        primarySwatch: Colors.green,
+//        visualDensity: VisualDensity.adaptivePlatformDensity,
+//      ),
+//      home: MyHomePage(title: 'Thieu Mao'),
+//    );
+//  }
+//}
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);

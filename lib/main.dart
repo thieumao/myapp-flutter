3f0821948d3b9c1a744993d3ede3af97746e9647
Thieu Nguyen <thieu@bluegrassdigital.com>
2020-07-06 14:23:37 +0700

    Use StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -4,80 +4,37 @@ import 'package:english_words/english_words.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final wordPair = new WordPair.random();
+  final wordPair = WordPair.random();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Learning Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Title'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Title'),
         ),
-        body: new Center(
-          child: new Text(
-              wordPair.asPascalCase,
-            style: new TextStyle(fontSize: 30),
-          ),
+        body: Center(
+          child: RandonEnglishWords(),
         )
       ),
     );
   }
 }
 
-//class MyApp extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return MaterialApp(
-//      title: 'Learning Flutter',
-//      theme: ThemeData(
-//        primarySwatch: Colors.green,
-//        visualDensity: VisualDensity.adaptivePlatformDensity,
-//      ),
-//      home: MyHomePage(title: 'Thieu Mao'),
-//    );
-//  }
-//}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class RandonEnglishWords extends StatefulWidget {
+  RandonEnglishWords({Key key, this.title}) : super(key: key);
   final String title;
   @override
-  MyHomePageState createState() => MyHomePageState();
+  RandonEnglishWordsState createState() => RandonEnglishWordsState();
 }
 
-class MyHomePageState extends State<MyHomePage> {
-  int counter = 0;
-  void incrementCounter() {
-    setState(() {
-      counter++;
-    });
-  }
-
+class RandonEnglishWordsState extends State<RandonEnglishWords> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Numbers',
-            ),
-            Text(
-              '$counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+    final wordPair = WordPair.random();
+    return Text(
+      wordPair.asPascalCase,
+      style: TextStyle(fontSize: 40),
     );
   }
 }
